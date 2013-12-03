273f9f21bc0555aa8ddca3acd415fb9238557756
Victor Lourng <hello@victorlourng.com>
2013-12-02 23:42:04 -0500

    Added basic room assets

---
+++ room-captures-verge/turntable_last_day_party_files/acc.js
@@ -0,0 +1,8 @@
+// Qualaroo for turntable.fm
+// (C) 2013 Qualaroo. All rights reserved.
+// qualaroo.com
+
+//$ site: 41740, generated: 2013-12-02 04:35:35 UTC
+//$ type: base, rev. f86be212 (deploy)
+
+//Your Qualaroo account is suspended or disabled.  Please see your dashboard at https://app.qualaroo.com (or email support@qualaroo.com).
